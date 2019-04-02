dca5d6c2d910f004fb8bcffdf31c841fcda33f19
Brian <riceb53@gmail.com>
2019-04-01 20:10:24 -0500

    add mathjs

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+var moment = require('moment');
+var math = require('mathjs');
+
 console.log('inside index.js');
+console.log('inside index.js and it is cool');
 
 
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+console.log(math.sqrt(-4)) // 2i
